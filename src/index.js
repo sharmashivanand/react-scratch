18fc7af238300456699eb54d21a0d1d2e3e8d30e
root <root@packer-debian-8-amd64.droplet.local>
2018-10-03 12:33:27 +0000

    First working build with react CSR

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-document.writeln('hello');
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('App'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,32 @@
+const webpack = require('webpack');
+
 module.exports = {
     context: __dirname + "/src",
     entry: "./index.js",
-  
+
     output: {
-      filename: "bundle.js",
-      path: __dirname + "/dist",
+        filename: "bundle.js",
+        //path: __dirname + "dist",
+        publicPath: __dirname + "/src"
     },
     devServer: {
         host: '0.0.0.0',//your ip address
         port: 8080,
+        contentBase: "dist",
         
     },
-    mode: "none"
-  }
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+                //loader: "babel-loader",
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    mode: "development"
+}
